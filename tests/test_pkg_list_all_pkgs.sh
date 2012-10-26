34a65be06ee4c5a1e7444687247cb18f834711a2
Aleksey Cheusov <vle@gmx.net>
2012-10-26 19:46:13 +0300

    pkg_list_all_pkgs: new options -a and -d

--- tests/test_pkg_list_all_pkgs.sh
+++ tests/test_pkg_list_all_pkgs.sh
@@ -11,3 +11,38 @@ END {
 cmp 'pkg_list_all_pkgs #1' \
 'XXXX
 '
+
+# pkg_list_all_pkgs -a
+env  pkg_list_all_pkgs -a devel |
+awk '
+/^devel/ {
+    ++cnt
+    next
+}
+END {
+    if (cnt > 10 && cnt != NR) {
+        print "XXXX"
+    }else{
+        print "failure"
+    }
+}' |
+cmp 'pkg_list_all_pkgs -a #2' \
+'XXXX
+'
+
+# pkg_list_all_pkgs -d
+env  pkg_list_all_pkgs -d devel |
+awk '
+/^devel/ {
+    ++cnt
+}
+END {
+    if (cnt > 10 && cnt == NR) {
+        print "XXXX"
+    }else{
+        print "failure"
+    }
+}' |
+cmp 'pkg_list_all_pkgs -d #3' \
+'XXXX
+'
