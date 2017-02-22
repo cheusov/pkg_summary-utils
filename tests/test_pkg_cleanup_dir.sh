faf4ac9e9f0efb86d257c20b2d65bc1641bbff0e
Aleksey Cheusov <vle@gmx.net>
2017-02-22 12:50:44 +0300

    pkg_cleanup_dir: new option -I was added

--- tests/test_pkg_cleanup_dir.sh
+++ tests/test_pkg_cleanup_dir.sh
@@ -78,9 +78,69 @@ done
 
 {
     pkg_cleanup_dir -d "$files_tempdir/" -f ALLSRCFILES \
-		    -r src_summary16.txt 1>&2
-    cd "$files_tempdir"
-    find .
-} | sort | cmp 'pkg_cleanup_dir #5' \
+		    -r src_summary16.txt 2>&1
+    ( cd "$files_tempdir"; find .; )
+} | sort |
+cmp 'pkg_cleanup_dir #5' \
 '.
+'
+
+#
+create_subdirs (){
+    rm -rf "$files_tempdir"
+    mkdir "$files_tempdir"
+    mkdir "$files_tempdir/misc" "$files_tempdir/devel" "$files_tempdir/All"
+
+    runawk -F= -v files_tempdir="$files_tempdir" -e '$1 == "FILE_NAME" {
+	fn = $2
+	print "" > (files_tempdir "/All/" fn)
+	print "" > (files_tempdir "/devel/" fn)
+	print "" > (files_tempdir "/misc/" fn)
+    }' bin_summary10.txt bin_summary11.txt bin_summary12.txt
+}
+
+# pkg_cleanup_dir #6
+create_subdirs
+{
+    pkg_cleanup_dir -r -d "$files_tempdir" -f FILE_NAME \
+	bin_summary10.txt bin_summary11.txt bin_summary12.txt 2>&1
+    ( cd "$files_tempdir"; find .; )
+} | sort |
+cmp 'pkg_cleanup_dir #6' \
+'.
+'
+
+# pkg_cleanup_dir #7
+create_subdirs
+{
+    pkg_cleanup_dir -r -d "$files_tempdir" -f FILE_NAME \
+	-I bin_summary10.txt bin_summary11.txt bin_summary12.txt 2>&1
+    ( cd "$files_tempdir"; find .; )
+} | sort |
+cmp 'pkg_cleanup_dir #7' \
+'.
+./All
+./All/farsight2-0.0.26nb6.tgz
+./All/gcc48-cc++-4.8.3.tgz
+./All/gcc48-libs-4.8.3.tgz
+./All/py26-gtk2-2.24.0nb4.tgz
+./All/vim-7.2.446nb1.tgz
+./All/vim-xaw-7.2.446nb1.tgz
+./All/webkit-gtk-2.4.5.tgz
+./devel
+./devel/farsight2-0.0.26nb6.tgz
+./devel/gcc48-cc++-4.8.3.tgz
+./devel/gcc48-libs-4.8.3.tgz
+./devel/py26-gtk2-2.24.0nb4.tgz
+./devel/vim-7.2.446nb1.tgz
+./devel/vim-xaw-7.2.446nb1.tgz
+./devel/webkit-gtk-2.4.5.tgz
+./misc
+./misc/farsight2-0.0.26nb6.tgz
+./misc/gcc48-cc++-4.8.3.tgz
+./misc/gcc48-libs-4.8.3.tgz
+./misc/py26-gtk2-2.24.0nb4.tgz
+./misc/vim-7.2.446nb1.tgz
+./misc/vim-xaw-7.2.446nb1.tgz
+./misc/webkit-gtk-2.4.5.tgz
 '
