6bd2fc2330448577272ea9b84ea9e6a75cea344c
Aleksey Cheusov <vle@gmx.net>
2017-02-23 20:21:54 +0300

    pkg_cleanup_packages: new option -d was added

--- tests/test_pkg_cleanup_packages.sh
+++ tests/test_pkg_cleanup_packages.sh
@@ -16,12 +16,32 @@ done
 PACKAGES="$files_tempdir"
 export PACKAGES
 
-# pkg_cleanup_distdir #1
+# pkg_cleanup_distdir #1.1
 {
     pkg_cleanup_packages -r bin_summary10.txt bin_summary11.txt 1>&2
     cd "$files_tempdir"
     find . | sed 's|^[.]/||'
-} | sort | cmp 'pkg_cleanup_packages #1' \
+} | sort | cmp 'pkg_cleanup_packages #1.1' \
+'.
+farsight2-0.0.26nb6.tgz
+pkg_summary.bz2
+pkg_summary.gz
+pkg_summary.txt
+py26-gtk2-2.24.0nb4.tgz
+vim-7.2.446nb1.tgz
+vim-xaw-7.2.446nb1.tgz
+'
+
+# pkg_cleanup_distdir #1.2
+{
+    (
+	unset PACKAGES
+	pkg_cleanup_packages -d "$files_tempdir" \
+			     -r bin_summary10.txt bin_summary11.txt 1>&2
+    )
+    cd "$files_tempdir"
+    find . | sed 's|^[.]/||'
+} | sort | cmp 'pkg_cleanup_packages #1.2' \
 '.
 farsight2-0.0.26nb6.tgz
 pkg_summary.bz2
