7b66ac941b4cc956b659077a89bc5d79b6b42f0c
Aleksey Cheusov <vle@gmx.net>
2017-02-23 20:44:43 +0300

    pkg_cleanup_distdir: new option -d was added

--- tests/test_pkg_cleanup_distdir.sh
+++ tests/test_pkg_cleanup_distdir.sh
@@ -17,12 +17,32 @@ done
 DISTDIR="$files_tempdir"
 export DISTDIR
 
-# pkg_cleanup_distdir #1
+# pkg_cleanup_distdir #1.1
 {
     pkg_cleanup_distdir -r src_summary.txt 1>&2
     cd "$files_tempdir"
     find . | sed 's|^[.]/||'
-} | sort | cmp 'pkg_cleanup_distdir #1' \
+} | sort | cmp 'pkg_cleanup_distdir #1.1' \
+'.
+ccc-subdir
+ccc-subdir/ccc-0.0.patch1
+ccc-subdir/ccc-0.0.patch2
+ccc-subdir/ccc-0.0.tgz
+ccc1-subdir
+ccc1-subdir/ccc-1.1.patch1
+ccc1-subdir/ccc-1.1.patch2
+ccc1-subdir/ccc-1.1.tgz
+'
+
+# pkg_cleanup_distdir #1.2
+{
+    (
+	unset DISTDIR
+	pkg_cleanup_distdir -d "$files_tempdir" -r src_summary.txt 1>&2
+    )
+    cd "$files_tempdir"
+    find . | sed 's|^[.]/||'
+} | sort | cmp 'pkg_cleanup_distdir #1.2' \
 '.
 ccc-subdir
 ccc-subdir/ccc-0.0.patch1
