fde1d7d43f19d809d5a1c10cb61201f598ec013c
Aleksey Cheusov <vle@gmx.net>
2023-11-26 21:06:31 +0400

    pkg_summary2leaves: add more tests

--- tests/test_pkg_summary2leaves.sh
+++ tests/test_pkg_summary2leaves.sh
@@ -26,6 +26,13 @@ wip/mk-configure
 wip/pkg_status
 '
 
+pkg_summary2leaves -pu bin_summary2.txt | sort |
+cmp 'pkg_summary2leaves #3.1' \
+'textproc/dict-client
+textproc/dict-server
+wip/pkgnih
+'
+
 pkg_summary2leaves -pva bin_summary2.txt | sort |
 cmp 'pkg_summary2leaves #4' \
 'devel/libmaa
@@ -39,7 +46,20 @@ wip/pkgnih
 wip/runawk
 '
 
-pkg_summary2leaves -pr bin_summary2.txt | sort |
+pkg_summary2leaves -pvu bin_summary2.txt | sort |
+cmp 'pkg_summary2leaves #4.1' \
+'devel/libmaa
+devel/pipestatus
+pkgtools/digest
+wip/distbb
+wip/mk-configure
+wip/paexec
+wip/pkg_status
+wip/pkg_summary-utils
+wip/runawk
+'
+
+pkg_summary2leaves -pra bin_summary2.txt | sort |
 cmp 'pkg_summary2leaves #5' \
 'pkgtools/digest
 wip/distbb
@@ -50,13 +70,33 @@ wip/pkg_summary-utils
 wip/runawk
 '
 
-pkg_summary2leaves -prv bin_summary2.txt | sort |
+pkg_summary2leaves -pru bin_summary2.txt | sort |
+cmp 'pkg_summary2leaves #5.1' \
+'textproc/dict-client
+textproc/dict-server
+wip/pkgnih
+'
+
+pkg_summary2leaves -prav bin_summary2.txt | sort |
 cmp 'pkg_summary2leaves #6' \
 'devel/libmaa
 devel/pipestatus
 textproc/dict-client
 textproc/dict-server
 wip/pkgnih
+'
+
+pkg_summary2leaves -pruv bin_summary2.txt | sort |
+cmp 'pkg_summary2leaves #6.1' \
+'devel/libmaa
+devel/pipestatus
+pkgtools/digest
+wip/distbb
+wip/mk-configure
+wip/paexec
+wip/pkg_status
+wip/pkg_summary-utils
+wip/runawk
 '
 
 pkg_summary2leaves -a bin_summary2.txt |
@@ -82,8 +122,29 @@ automatic=yes
 
 '
 
+pkg_summary2leaves -u bin_summary2.txt |
+cmp 'pkg_summary2leaves #7.1' \
+'PKGPATH=textproc/dict-client
+PKGNAME=dict-client-1.11.2
+DEPENDS=libmaa>=1.2.0
+
+PKGPATH=textproc/dict-server
+PKGNAME=dict-server-1.11.2
+DEPENDS=libmaa>=1.2.0
+
+PKGNAME=pkgnih-0.3.1
+BUILD_DATE=2011-01-18 13:27:32 +0200
+PKGPATH=wip/pkgnih
+
+'
+
 pkg_summary2leaves -a 2>&1 |
 cmp 'pkg_summary2leaves #8' \
+'pkg_summary2leaves requires <files...>
+'
+
+pkg_summary2leaves -u 2>&1 |
+cmp 'pkg_summary2leaves #8.1' \
 'pkg_summary2leaves requires <files...>
 '
 
@@ -102,7 +163,35 @@ automatic=yes
 
 '
 
-pkg_summary2leaves -ap bin_summary11.txt |
+pkg_summary2leaves -u bin_summary11.txt |
 cmp 'pkg_summary2leaves #10.2' \
+'PKGNAME=pidgin-2.10.3
+DEPENDS=farsight2>=0.0.26nb6
+COMMENT=Multi-protocol Instant Messaging client GTK frontend
+BUILD_DATE=2012-04-19 04:00:06 +0000
+PKGPATH=chat/pidgin
+
+'
+
+pkg_summary2leaves -ap bin_summary11.txt |
+cmp 'pkg_summary2leaves #11.1' \
 'x11/py-gtk2
 '
+
+pkg_summary2leaves -up bin_summary11.txt |
+cmp 'pkg_summary2leaves #11.2' \
+'chat/pidgin
+'
+
+pkg_summary2leaves -apr bin_summary11.txt |
+cmp 'pkg_summary2leaves #12.1' \
+'x11/py-gtk2
+'
+
+pkg_summary2leaves -upr bin_summary11.txt |
+cmp 'pkg_summary2leaves #12.2' \
+'chat/pidgin
+multimedia/farsight2
+x11/py-gtk2
+'
+
